a753747a2b5c4a673efd80cf92c97a897d491378
PutuGandi <putugandi12@gmail.com>
2020-02-02 23:42:20 +0800

    Done

--- automate.sh
+++ automate.sh
@@ -14,13 +14,13 @@ db_user=sudo grep -E "^db_user" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
 sudo sed -i s/devopscilsy/$db_user/g config.php
 
 db_pass=sudo grep -E "^db_pass" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
-sudo sed -i s/123/$db_pass/g config.php
+sudo sed -i s/1234567890/$db_pass/g config.php
 
 db_name=sudo grep -E "^db_name" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
 sudo sed -i s/dbsosmed/$db_user/g config.php
 
-sudo cat default > /etc/nginx/sites-enabled/default
-cd /etc/nginx/sites-enabled/
+#sudo cat default > /etc/nginx/sites-enabled/default
+sudo vim /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo systemctl reload nginx
 cd /var/www/html/
@@ -28,7 +28,7 @@ echo ""
 echo "==================="
 echo "MASUKKAN PASS MYSQL"
 echo "==================+"
-sudo mysql -u $db_user -p $db_pass < dump.sql
+sudo mysql -u $db_user -p $db_name < dump.sql
 echo ""
 echo "---------"
 echo " SELESAI "
--- mysql_setup.sh
+++ mysql_setup.sh
@@ -5,21 +5,19 @@ check="Select user,host from mysql.user;"
 db_user="jopan"
 db_pass="123"
 db_name="Devops"
-sudo $MYSQL -u root -p -e "$check" | grep -E "$db_user"
+sudo $MYSQL -uroot -p -e "$check" | grep -E "$db_user"
 if [ $? -ne 0 ]; then
-	echo ""
+	echo " "
 	echo "ANDA AKAN DIARAHKAN MEMBUAT USER DAN NAMA DATABASE"
-	echo " ENTER SAJA "
-	Q1="CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_pass';"
-	Q2="GRANT ALL ON *.* TO '$db_user'@'localhost';"
+	echo "ENTER SAJA"
+	Q1="CREATE DATABASE IF NOT EXISTS $db_name;"
+	Q2="GRANT ALL ON *.* TO '$db_user'@'localhost' IDENTIFIED BY '$db_pass';"
 	Q3="FLUSH PRIVILEGES;"
 	SQL="${Q1}${Q2}${Q3}"
-	sudo $MYSQL -u root -p -e "$SQL"
-	K1="CREATE DATABASE '$db_name';"
-	SQL2="${K1}"
-	sudo $MYSQL -u $db_user -p -e "$SQL2"
+	sudo $MYSQL -uroot -p -e "$SQL"
 	echo "Database '$db_name' dan user '$db_user' sudah dibuat dengan password-nya"
 else
 	echo "USERNAME SUDAH ADA"
 fi
+
 

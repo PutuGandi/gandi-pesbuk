d9bfeb399ab0c653c981551ce8aa2b835ff6ead0
PutuGandi <putugandi12@gmail.com>
2020-02-02 22:24:42 +0800

    Fourth

--- automate.sh
+++ automate.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install unzip
+#sudo apt install unzip
 sudo apt install nginx
 sudo apt install mysql-server
 ./mysql_setup.sh
 sudo apt install php-fpm php-mysql
-sudo wget https://github.com/PutuGandi/gandi-pesbuk/archive/master.zip
-unzip master.zip
+#sudo wget https://github.com/PutuGandi/gandi-pesbuk/archive/master.zip
+#unzip master.zip
 sudo rm -rf /var/www/html/*
 sudo cp gandi-pesbuk-master/media-sosial/* /var/www/html/
 db_user=sudo grep -E "^db_user" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
-sudo sed -i s/gandi/$db_user/g config.php
+sudo sed -i s/devopscilsy/$db_user/g config.php
 
 db_pass=sudo grep -E "^db_pass" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
 sudo sed -i s/123/$db_pass/g config.php
 
 db_name=sudo grep -E "^db_name" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
-sudo sed -i s/dbpesbuk/$db_user/g config.php
+sudo sed -i s/dbsosmed/$db_user/g config.php
 
 sudo cat default > /etc/nginx/sites-enabled/default
 cd /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl reload nginx
+cd /var/www/html/
 echo ""
 echo "==================="
 echo "MASUKKAN PASS MYSQL"

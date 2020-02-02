6fcc7b0f33d1188233f66738f919654c2c683ef2
PutuGandi <putugandi12@gmail.com>
2020-02-02 21:35:57 +0800

    Fourth

--- automate.sh
+++ automate.sh
@@ -11,21 +11,25 @@ unzip master.zip
 sudo rm -rf /var/www/html/*
 sudo cp gandi-pesbuk-master/media-sosial/* /var/www/html/
 db_user=sudo grep -E "^db_user" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
-sudo sed -i s/jopan/$db_user/g config.php
+sudo sed -i s/gandi/$db_user/g config.php
 
 db_pass=sudo grep -E "^db_pass" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
 sudo sed -i s/123/$db_pass/g config.php
 
 db_name=sudo grep -E "^db_name" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
-sudo sed -i s/Devops/$db_user/g config.php
+sudo sed -i s/dbpesbuk/$db_user/g config.php
 
 sudo cat default > /etc/nginx/sites-enabled/default
 cd /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl reload nginx
-echo""
-echo"==================="
-echo"MASUKKAN PASS MYSQL"
-echo"==================+"
+echo ""
+echo "==================="
+echo "MASUKKAN PASS MYSQL"
+echo "==================+"
 sudo mysql -u $db_user -p $db_pass < dump.sql
+echo ""
+echo "---------"
+echo " SELESAI "
+echo "---------"
 

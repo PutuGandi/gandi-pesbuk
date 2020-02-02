0a5a15432887d92604944e5e317707538bc1ddcc
PutuGandi <putugandi12@gmail.com>
2020-02-02 21:28:17 +0800

    Third

--- automate.sh
+++ automate.sh
@@ -11,5 +11,21 @@ unzip master.zip
 sudo rm -rf /var/www/html/*
 sudo cp gandi-pesbuk-master/media-sosial/* /var/www/html/
 db_user=sudo grep -E "^db_user" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
-sudo sed -i s/jopan/$db_user/g config.php 
+sudo sed -i s/jopan/$db_user/g config.php
+
+db_pass=sudo grep -E "^db_pass" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
+sudo sed -i s/123/$db_pass/g config.php
+
+db_name=sudo grep -E "^db_name" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2
+sudo sed -i s/Devops/$db_user/g config.php
+
+sudo cat default > /etc/nginx/sites-enabled/default
+cd /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl reload nginx
+echo""
+echo"==================="
+echo"MASUKKAN PASS MYSQL"
+echo"==================+"
+sudo mysql -u $db_user -p $db_pass < dump.sql
 

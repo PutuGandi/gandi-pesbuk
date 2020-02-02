2edd271093c140a17ebe8662f3343f09c50660b2
PutuGandi <putugandi12@gmail.com>
2020-02-03 01:25:58 +0800

    DONE

--- automate.sh
+++ automate.sh
@@ -12,13 +12,13 @@ sudo rm -rf /var/www/html/*
 sudo cp -r gandi-pesbuk-master/media-sosial/* /var/www/html/
 
 db_user=`sudo grep -E "^db_user" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2`
-sudo sed -i s/devopscilsy/"$db_user"/g /var/www/html/config.php
+sudo sed -i s/devopscilsy/$db_user/g /var/www/html/config.php
 
 db_pass=`sudo grep -E "^db_pass" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2`
-sudo sed -i s/1234567890/"$db_pass"/g /var/www/html/config.php
+sudo sed -i s/1234567890/$db_pass/g /var/www/html/config.php
 
 db_name=`sudo grep -E "^db_name" mysql_setup.sh | cut -d\= -f2 | cut -d\" -f2`
-sudo sed -i s/dbsosmed/"$db_name"/g /var/www/html/config.php
+sudo sed -i s/dbsosmed/$db_name/g /var/www/html/config.php
 
 #sudo cat default > /etc/nginx/sites-enabled/default
 sudo vim /etc/nginx/sites-enabled/default
@@ -29,7 +29,7 @@ echo ""
 echo "==================="
 echo "MASUKKAN PASS MYSQL"
 echo "==================="
-sudo mysql -u "$db_user" -p "$db_name" < dump.sql
+sudo mysql -u $db_user -p $db_name < dump.sql
 echo ""
 echo "---------"
 echo " SELESAI "

6806c78af727a310d35e81e5ab7c98125c5336d7
PutuGandi <putugand12@gmail.com>
2020-02-09 10:59:17 +0800

    Fix

--- automate_aws.sh
+++ automate_aws.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+sudo apt update
+unzip master.zip
 sudo apt install nginx -y
 sudo apt install php-fpm php-mysql -y
 sudo rm -rf /var/www/html/*
+apt-get install awscli
+sudo apt-get install automake autotools-dev fuse g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+./autogen.sh
+./configure
+make
+sudo make install
+cd
+echo AKIAVFKGS24IZAYA7B4Q:x/6ih47M6s7W6znz3TWdF49npkltOFZiJlXmHSSv > /home/ubuntu/.passwd-s3fs
+sudo chmod 600 /home/ubuntu/.passwd-s3fs
+sudo s3fs SP2-Bucket /var/www/html -o passwd_file=/home/ubuntu/.passwd-s3fs
 sudo cp -r ~/gandi-pesbuk-master/media-sosial/* /var/www/html/
 sudo sed -i s/localhost/$1/g /var/www/html/config.php
 sudo sed -i s/devopscilsy/$2/g /var/www/html/config.php
 sudo sed -i s/1234567890/$3/g /var/www/html/config.php
 sudo sed -i s/dbsosmed/$4/g /var/www/html/config.php
-crontab schedule
 sudo unlink /etc/nginx/sites-enabled/default
 sudo cp -r ~/gandi-pesbuk-master/default /etc/nginx/sites-available/
 cd /etc/nginx/sites-enabled/
@@ -17,5 +30,3 @@ sudo nginx -t
 sudo systemctl reload nginx
 cd /var/www/html/
 sudo mysql -h $1 -u$2 -p$3 $4 < dump.sql
-
-
